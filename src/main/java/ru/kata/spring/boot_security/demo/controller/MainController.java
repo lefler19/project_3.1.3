ccb393b52561f799810bb45fb8f62a0e7bc54d69
Настя <west_2017@mail.ru>
2023-11-13 17:06:16 +0300

    work without LAZY

--- src/main/java/ru/kata/spring/boot_security/demo/controller/MainController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/MainController.java
@@ -4,12 +4,9 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.kata.spring.boot_security.demo.entities.User;
-import ru.kata.spring.boot_security.demo.services.UserService;
-//import ru.kata.spring.boot_security.demo.entities.User;
-//import ru.kata.spring.boot_security.demo.services.UserService;
 
-import java.security.Principal;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
 
 
 @Controller
